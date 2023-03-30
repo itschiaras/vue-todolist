f9db753f00ef442753e4c3c094fa0af79792c03f
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-30 16:18:47 +0200

    add toDoList array, template in html, v-for in li

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            toDoList: [
+                {
+                    name: 'Stigghiola',
+                    done: false
+                },
+                {
+                    name: 'Mangia e Bevi',
+                    done: false
+                },
+                {
+                    name: 'Carne di crasto',
+                    done: false
+                },
+                {
+                    name: 'Alette di pollo',
+                    done: false
+                },
+                {
+                    name: '20 filoni di pane',
+                    done: false
+                },
+                {
+                    name: '5 casse di birra Moretti',
+                    done: false
+                }
+            ]
         }
     },
     methods: {
